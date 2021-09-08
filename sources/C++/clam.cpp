4e883f592cfe7ce4f4b14afab70277bc2bf402c6
legojrp <84683257+legojrp@users.noreply.github.com>
2021-09-08 12:13:17 -0400

    C++ 2.0-beta.1.5

--- sources/C++/clam.cpp
+++ sources/C++/clam.cpp
@@ -2,34 +2,36 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+//grading system for each method is as follows
+//(overall grade).(debugged, 0 or 1).(speed / optimization grade).(Actual speed).(Organization Grade).(Error Protection).(documentation/explaination).(time of grading) >> (Any comments)
 class CLAM {
 public:
-	CLAM(std::string path, int CLAMfileversion = 1, std::string customKey = "$C**M$"){
+	CLAM(std::string path, int CLAMfileversion = 1, std::string customKey = "$C**M$"){// ---
     	this->path = path;
       	if (CLAMfileversion == 1){
         	
       }
 
     }
-	inline void writeToProperty(std::string Property, std::string Replacement);
-	inline std::string readFromProperty(std::string Property);
-	inline void createSubset();
-	inline void createProperty(std::string Property, std::string Replacement);
+	inline void writeToProperty(std::string Property, std::string Replacement); // ---
+	inline std::string readFromProperty(std::string Property);// ---
+	inline void createSubset();// ---
+	inline void createProperty(std::string Property, std::string Replacement);//--- 
 private:
-	std::string path;
-	int CLAMfileversion;
-	std::string customKey;
+	std::string path;//
+	int CLAMfileversion;//
+	std::string customKey;//
 	//.clam
-	inline void clam_writeToProperty(std::string Property, std::string Replacement);
-	inline std::string clam_readFromProperty(std::string Property);
-	inline void clam_createProperty(std::string Property, std::string Replacement);
+	inline void clam_writeToProperty(std::string Property, std::string Replacement);// 2.0.3.__.4.2.0.(8.9.21) >> good -- could be optimized, with the deleting of the file and reopening. Speed test coming sooon
+	inline std::string clam_readFromProperty(std::string Property);// 5.0.8.__.9.2.0.(8.9.21)
+	inline void clam_createProperty(std::string Property, std::string Replacement);//5.0.9.__.9.1.0(8.9.21)
 	//.clam2
-	inline std::string clam2_getCustomKey();
-	inline void clam2_writeToProperty(std::string Property, std::string Replacement);
-	inline std::string clam2_readFromProperty(std::string Property);
-	inline void clam2_createProperty(std::string Property, std::string Replacement);
-	inline int clam2_changeCustomKey(std::string key);
-	inline int createPassword(std::string password);
+	inline std::string clam2_getCustomKey();//
+	inline void clam2_writeToProperty(std::string Property, std::string Replacement);//
+	inline std::string clam2_readFromProperty(std::string Property);//
+	inline void clam2_createProperty(std::string Property, std::string Replacement);//
+	inline int clam2_changeCustomKey(std::string key);//
+	inline int createPassword(std::string password);//
 
 };
 //ALL
@@ -42,16 +44,18 @@ private:
 *
 *
 */
-std::string CLAM::readFromProperty(std::string Property){
-//for later C++ 2.0-beta.2.0
-}
-void CLAM::writeToProperty(std::string Property, std::string Replacement){
-}
-void CLAM::createProperty(std::string Property, std::string Replacement){
-  	std::ofstream file(this->path);
-    file << Property + ": " + Replacement + "\n";
-    file.close();
-}
+	std::string CLAM::readFromProperty(std::string Property){
+		//for later C++ 2.0-beta.2.0
+	}
+
+	void CLAM::writeToProperty(std::string Property, std::string Replacement){
+	}
+
+	void CLAM::createProperty(std::string Property, std::string Replacement){
+  		std::ofstream file(this->path);
+    		file << Property + ": " + Replacement + "\n";
+    		file.close();
+	}
 
 
 
@@ -71,14 +75,14 @@ void CLAM::createProperty(std::string Property, std::string Replacement){
  		std::ifstream file(this->path);
   		std::string text;
   		std::string returnstring = "";
-   			while (std::getline(file, text)){
-				if (text.substr(0, text.find(": "))== Property){
-					returnstring = text.erase(0, Property.length() + 1);
+   		while (std::getline(file, text)){
+			if (text.substr(0, text.find(": "))== Property){
+				returnstring = text.erase(0, Property.length() + 1);
     			}
-			}
+		}
   		if (returnstring == ""){
-  				returnstring = "Error";
-  			}
+  			returnstring = "Error";
+  		}
   		return returnstring;
 
 	}
@@ -87,32 +91,32 @@ void CLAM::createProperty(std::string Property, std::string Replacement){
 	void CLAM::clam_writeToProperty(std::string Property, std::string Replacement){
 		std::ifstream file(this->path);
   		std::string text, pre, post, changed;
-    	int preOrPost = 0;
-    	while (std::getline(file, text)){
-      		if (text.substr(0, text.find(": ")) == Property){
-         		preOrPost= 1;
-        		continue;
-      		}
-      		else if ( preOrPost == 0){
-        		pre += text + "\n";
-      		}
-      		else if (preOrPost == 1){
-        		post += text + "\n";
-      		}
+    		int preOrPost = 0;
+    		while (std::getline(file, text)){
+      			if (text.substr(0, text.find(this->customKey)) == Property){
+         			preOrPost= 1;
+        			continue;
+      			}
+      			else if (preOrPost == 0){
+        			pre += text + "\n";
+      			}
+      			else if (preOrPost == 1){
+        			post += text + "\n";
+      			}
 
-    	}
-    	changed = pre + Property + ": " + Replacement + "\n" + post;
-    	file.close();
-    	std::remove(this->path.c_str());
-    	std::ofstream file2(this->path);
-    	file2 << changed;
-    	file2.close();
-}
+    		}
+    		changed = pre + Property + this->customKey + Replacement + "\n" + post;
+    		file.close();
+    		std::remove(this->path.c_str());
+    		std::ofstream file2(this->path);
+    		file2 << changed;
+    		file2.close();
+	}
 	void CLAM::clam_createProperty(std::string Property, std::string Replacement){
   		std::ofstream file(this->path);
-    	file << Property + ": " + Replacement + "\n";
-    	file.close();
-}
+    		file << Property + this->customKey + Replacement + "\n";
+    		file.close();
+	}
 //CLAM2
 /*
 *
@@ -122,29 +126,28 @@ void CLAM::createProperty(std::string Property, std::string Replacement){
 *
 *
 */
-    std::string CLAM::clam2_getCustomKey(){
+	std::string CLAM::clam2_getCustomKey(){ /// +3 error, -2 error, + 4 organization
 		std::ifstream file(this->path);
-     	if (this->CLAMfileversion != 2){
-          return "ERROR";
-        }
+     		if (this->CLAMfileversion != 2){
+        		return "ERROR";
+       		}
+		std::string Property = t
   		std::string text;
   		std::string returnstring = "";
    		while (std::getline(file, text)){
-			if (text.substr(0, text.find(": "))== Property){
+			if (text.substr(0, text.find(": ")) == Property){
 				returnstring = text.erase(0, Property.length() + 1);
-              if (Property == "CustomKey"){
-					this->customKey = returnstring;
-              }
-              else {
+				this->customKey = returnstring;
+              			else {
 					continue;
-              }
+              			}
               
-    		}
-          else {
-            if (text.find("///")){
-              return "Error";
-            }
-          }
+    			}
+          		else {
+            			if (text.find("///")){
+              				return "$C**M$ e: 1.2.1";
+            			}
+          		}
 		}
   		if (returnstring == ""){
   			returnstring = "Error";
