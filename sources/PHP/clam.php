e0ff1f5af0e36ac2b95fa6c582f1c53797eadf54
legojrp <84683257+legojrp@users.noreply.github.com>
2021-09-28 21:36:24 -0400

    Update clam.php

--- sources/PHP/clam.php
+++ sources/PHP/clam.php
@@ -1,1 +1,20 @@
-
+<?php 
+  class CLAM {
+  private $path
+  function __construct($path){
+    $this->path = $path;
+  }
+   function writeToProperty($property, $replacement){
+       $file = fopen($this->path);
+       $
+   }
+   function readFromProperty($property){
+  
+   }
+   function createProperty($property, $content){
+        $file = fopen($this->path, "a"){
+        fwrite($file, $property ) 
+   }
+   
+  }
+?> 
