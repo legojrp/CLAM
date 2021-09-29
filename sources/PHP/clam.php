d70067eace8f928f0ab9b1ae8db7e9db87e2db9a
legojrp <84683257+legojrp@users.noreply.github.com>
2021-09-29 18:12:35 -0400

    Update clam.php

--- sources/PHP/clam.php
+++ sources/PHP/clam.php
@@ -6,13 +6,15 @@
   }
    function writeToProperty($property, $replacement){
        $file = fopen($this->path, "r");
-  		 $text, $pre, $post, $changed;
+  		 $text;
+       $pre;
+        $post;
+       $changed;
     	 $preOrPost = 0;
     		while (feof($file)){
             $text = fgets($file);
-      			if (substr($text, 0, find($text, ": ") == property){
+      			if (substr($text, 0, find($text, ": ")) == $property){
          			$preOrPost = 1;
-        			continue;
       			}
       			else if ($preOrPost == 0){
         			$pre += $text . "\n";
@@ -24,33 +26,36 @@
     		}
     		$changed = $pre . $property . ": " . $replacement + "\n" . $post;
     		fclose($file);
-    		$file fopen($this->path, "");
+    		$file = fopen($this->path, "w+");
     		fwrite($file, $changed);
     		fclose($file);
    }
+
+
    function readFromProperty($property){
    $file = fopen($this->path, "r");
    $text = "";
    $returnstring = "";
    while (!feof($file)){
    $text = fgets($file);
-   $substr = substr($text,0, find($text, ": "))
+   $substr = substr($text,0, find($text, ": "));
    if ($substr == $property){
       $returnstring = substr($text, strlen($substr) + 2);
       
    }
-    else ($substr != $property) {
-    $returnstring = null;
+    else if ($substr !== $property) {
+    $returnstring = "";
     }
     
    }
      return $returnstring;
    }
    function createProperty($property, $content){
-        $file = fopen($this->path, "a"){
+        $file = fopen($this->path, "a");
         fwrite($file, $property . ": " . $content . "\n");
         fclose($file);
    }
    
   }
-?> 
+
+?>
